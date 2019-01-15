58ab8a1287b51c11caf6ad39b0348c1833103ad8
Celeste Rex <you@example.com>
2019-01-15 21:01:30 +0100

    Migration of data bases

--- db/migrate/20190115192254_create_contacts_table.rb
+++ db/migrate/20190115192254_create_contacts_table.rb
@@ -1,7 +1,7 @@
 class CreateContactsTable < ActiveRecord::Migration[5.2]
   def change
     create_table :contacts do |t|
-      t.string :names 
+      t.string :name
     end
   end
 end
--- db/migrate/20190115193242_create_phone_number.rb
+++ db/migrate/20190115193242_create_phone_number.rb
@@ -1,6 +1,8 @@
 class CreatePhoneNumber < ActiveRecord::Migration[5.2]
   def change
     create_table :phone_numbers do |t|
+      t.integer :contact_id
+      t.string :number
     end
   end
 end
